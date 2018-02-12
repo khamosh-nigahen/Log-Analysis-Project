12225bbc52b1e2b0b219e53c5bb7987f22a93e25
Puneet Jain <mailpuneet8264@gmail.com>
2018-02-12 21:09:10 +0530

    imported DB module and initialised variables

---
+++ reporting_tool.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+# Database Name
+DB_NAME = "news"
+
+# Filename 
+FILENAME = "log_analysis.txt"
+
+#main starts
+if __name__ == "__main__":
+    print("Fetching the data from the Database...")
+
