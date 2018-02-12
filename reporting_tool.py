fbc8f77498c160d847b098240ae3250db49777d3
Puneet Jain <mailpuneet8264@gmail.com>
2018-02-12 21:13:39 +0530

    Added queries, dictionary to store data

--- reporting_tool.py
+++ reporting_tool.py
@@ -8,6 +8,24 @@ DB_NAME = "news"
 # Filename 
 FILENAME = "log_analysis.txt"
 
+# queries 
+first_query = "select title,views from view_article limit 3"
+
+second_query = "select * from view_author"
+
+third_query = "select * from view_error_log where percent_error > 1"
+
+# to store results
+first_query_dict = dict()
+first_query_dict['title'] = "\n1. The 3 most popular articles of all time are:\n"
+
+second_query_dict = dict()
+second_query_dict['title'] = "\n2. The most popular article authors of all time are:\n"
+
+third_query_dict = dict()
+third_query_dict['title'] = "\n3. Days with more than 1% of request that lead to an error:\n"
+
+
 #main starts
 if __name__ == "__main__":
     print("Fetching the data from the Database...")
